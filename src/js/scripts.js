31ae161fa75b1f7eee00ba21629613e82edeef77
devhimal <himalhobbs.364@gmail.com>
2024-09-12 22:24:57 +0545

    feat: added stripi cms for the video section.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,7 +1,11 @@
 document.addEventListener("DOMContentLoaded", async function () {
   try {
-    const response = await fetch(`http://localhost:3000/api/videos`); // Ensure you use the correct port and route
+    const headers = {
+      'Authorization': 'Bearer f2de6a202cd9dd9b65c97c2c46e92c12ebbbaa1d0d56ce82e4441fe84042d77fd98e1946f120625fb7a526cc4477202e3c177f1b3b4e701f686956409d12ebc8e0bacd5d2d11d9e93797c5b4162d59d2a725d79927ea81105b287322700e4ed0509fb75bf1418fc5b48da8ab3b1c68dc13bc99f267d46c7ad1232a8b1e7cc646'
+    };
+    const response = await fetch('http://localhost:1337/api/videostd', { headers: headers }); // Ensure you use the correct port and route
     const videos = await response.json();
+    
     const videoContainer = document.getElementById("videoWrapper");
 
     if (!videoContainer) {
@@ -12,7 +16,7 @@ document.addEventListener("DOMContentLoaded", async function () {
     // Variable to track the currently playing video
     let currentPlayingVideo = null;
 
-    videos.forEach((video, index) => {
+    videos.data.forEach((video, index) => {
       const videoElement = document.createElement("div");
       videoElement.classList.add("video-card");
 
@@ -20,15 +24,15 @@ document.addEventListener("DOMContentLoaded", async function () {
       videoElement.innerHTML = `
         
         <div class="video-wrapper">
-          <video width="100%" height="500px" id="video${index}" class="custom-video" poster="poster.jpg">
-            <source src="../../public/videos/fireship.mp4" type="video/mp4">
+          <video width="100%" height="500px" id="video${index}" class="custom-video" poster=${video.attributes.banner_url}>
+            <source src=${video.attributes.video_url} type="video/mp4">
             Your browser does not support the video tag.
           </video>
           <div class="play-pause-btn" id="playPauseBtn${index}"></div>
         </div>
-        <img src=${video.videoIcons} alt="" style="margin: 20px 0px"/>
-        <h2 class="video-heading">${video.title}</h2>
-        <p class="video-paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
+        <img src=${video.attributes.icons_url} alt="" style="margin: 50px 0px 20px 0px;" class="videoIcons"/>
+        <h2 class="video-heading">${video?.attributes.video_name}</h2>
+        <p class="video-paragraph">${video?.attributes.description}</p>
       `;
 
       videoContainer.appendChild(videoElement);
